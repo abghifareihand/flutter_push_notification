fb709e1ef49aace914d7a9a60e9f20e2ad264123
abghifareihand <info.abghifareihan@gmail.com>
2024-04-30 19:01:20 +0700

    add package and notification service

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_push_notification/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'firebase_options.dart';
+import 'services/notification_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  if (await Permission.notification.request().isGranted) {
+    await NotificationService().initNotifications();
+    await NotificationService().getToken();
+  }
   runApp(const MyApp());
 }
 
